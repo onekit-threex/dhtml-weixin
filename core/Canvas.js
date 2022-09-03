8212d2b325bea26b957c22df92a7e7094e4ab32c
OneKit <onekit@onekit.cn>
2022-09-03 10:17:58 +0800

    .

--- core/Canvas.js
+++ core/Canvas.js
@@ -1,6 +1,9 @@
 export default class Canvas{
     
     static canvas2image(canvas3d, canvas2d) {
+        if(canvas3d.wx_element){
+            canvas3d = canvas3d.wx_element
+        }
         return new Promise((callback) => {
             var image = canvas2d.createImage()
             image.onload = function () {
