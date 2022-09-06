b4c1b49e61956a4ca08a8d96d590538935fa7a13
OneKit <onekit@onekit.cn>
2022-09-06 15:46:27 +0800

    .

--- Event.js
+++ Event.js
@@ -11,11 +11,14 @@ export default class Event {
         //
         if(wx_e.changedTouches.length>0){
             const touch = wx_e.changedTouches[0]
-            web_e.pointerId = 2//touch.identifier;
+            web_e.pointerId = touch.identifier || 2;
             web_e.pageX = touch.x;
             web_e.pageY = touch.y;
             web_e.clientX = touch.x-wx_e.currentTarget.offsetLeft;
             web_e.clientY = touch.y-wx_e.currentTarget.offsetTop;
+            //
+            web_e.offsetX = web_e.clientX;
+            web_e.offsetY = web_e.clientY;
             web_e.deltaX = web_e.offsetX;
             web_e.deltaY = web_e.offsetY;
         }
