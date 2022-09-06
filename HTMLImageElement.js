7d973bad95b95bb212e452fb806c8a767966d328
OneKit <onekit@onekit.cn>
2022-09-06 22:15:59 +0800

    .

--- HTMLImageElement.js
+++ HTMLImageElement.js
@@ -3,76 +3,77 @@
 /* eslint-disable no-mixed-operators */
 /* eslint-disable no-bitwise */
 /* eslint-disable no-cond-assign */
-import EventTarget from './EventTarget'
-import Base64  from "./core/Base64"
+import EventTarget from "./EventTarget";
+import Base64 from "./core/Base64";
 
 export default class HTMLImageElement extends EventTarget {
-  constructor() {
-    super()
-    const canvas = getApp().canvas
-    this.wx_image = canvas.createImage()
+  constructor(canvas2d) {
+    super();
+    const canvas = canvas2d || getApp().canvas;
+    this.wx_image = canvas.createImage();
     this.wx_image.onload = () => {
       if (this.onload) {
-        this.onload.call(this)
+        this.onload.call(this);
       }
       if (this._all_event_handlers.load) {
-        this._all_event_handlers.load.forEach(handler => {
-          handler.call(this)
-        })
+        this._all_event_handlers.load.forEach((handler) => {
+          handler.call(this);
+        });
       }
-    }
+    };
     this.wx_image.onerror = (e) => {
       if (this.onerror) {
-        this.onerror.call(this, e)
+        this.onerror.call(this, e);
       }
       if (this._all_event_handlers.error) {
-        this._all_event_handlers.error.forEach(handler => {
-          handler.call(this, e)
-        })
+        this._all_event_handlers.error.forEach((handler) => {
+          handler.call(this, e);
+        });
       }
-    }
-    this.onekit_image = this
+    };
+    this.onekit_image = this;
   }
   get width() {
-    return this.wx_image.width
+    return this.wx_image.width;
   }
   get height() {
-    return this.wx_image.height
+    return this.wx_image.height;
   }
   get data() {
-    return this.wx_image.data
+    return this.wx_image.data;
   }
   get complete() {
-    return this.wx_image.complete
+    return this.wx_image.complete;
   }
   set crossOrigin(crossOrigin) {
-    this._crossOrigin = crossOrigin
+    this._crossOrigin = crossOrigin;
   }
 
   get crossOrigin() {
-    return this._crossOrigin
+    return this._crossOrigin;
   }
 
   set src(src) {
-    if(getApp().onekit_debug){
-      console[getApp().onekit_debug]('[image]', src)
+    if (getApp().onekit_debug) {
+      console[getApp().onekit_debug]("[image]", src);
     }
-    this._src = src
+    this._src = src;
 
-     if (src.startsWith('blob:')) {
-      const array = getApp().ObjectURL[src].array[0]
-      this.wx_image.src = 'data:image/png;base64,' + Base64.arrayBuffer2Base64(array)
-     /* const filePath = wx.getStorageSync(src);
+    if (src.startsWith("blob:")) {
+      const array = getApp().ObjectURL[src].array[0];
+      this.wx_image.src =
+        "data:image/png;base64," + Base64.arrayBuffer2Base64(array);
+      /* const filePath = wx.getStorageSync(src);
       const fs = wx.getFileSystemManager()
       const base64 = fs.readFileSync(filePath, 'base64', 0)
       console.error(filePath)
       this.wx_image.src = 'data:image/png;base64,' +base64*/
     } else {
-      this.wx_image.src = src
+      this.wx_image.src = src;
     }
   }
 
   get src() {
-    return this._src
+    return this._src;
   }
 }
--- core/Canvas.js
+++ core/Canvas.js
@@ -1,3 +1,4 @@
+import HTMLImageElement from "../HTMLImageElement"
 export default class Canvas{
     
     static canvas2image(canvas3d, canvas2d=getApp().canvas2d) {
@@ -10,6 +11,18 @@ export default class Canvas{
                 callback(image)
             }
             image.src = canvas3d.toDataURL()
+        })
+    }
+    static canvas2img(canvas3d, canvas2d=getApp().canvas2d) {
+        if(canvas3d.wx_element){
+            canvas3d = canvas3d.wx_element
+        }
+        return new Promise((callback) => {
+            var img = new HTMLImageElement(canvas2d)
+            img.onload = function () {
+                callback(img)
+            }
+            img.src = canvas3d.toDataURL()
         })
     }
 }
