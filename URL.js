8a809580cf11ce18bba3649ac2316d1885f3cb33
OneKit <onekit@onekit.cn>
2022-09-06 18:14:33 +0800

    .

--- URL.js
+++ URL.js
@@ -4,19 +4,27 @@ export default class URL {
 	static createObjectURL(blob) {
 		const guid = GUID();
 		const url = `blob:http://localhost/${guid}`;
+		if(! getApp().ObjectURL){
+			getApp().ObjectURL = {}
+		}
+		getApp().ObjectURL[url] = blob
+		/*
 		const path = `${wx.env.USER_DATA_PATH}/${guid}.png`;
 		const fs = wx.getFileSystemManager()
 		fs.writeFileSync(path, blob.array[0],'base64')
-		wx.setStorageSync(url, path);
+		wx.setStorageSync(url, path);*/
+
 		return url;
 	}
 
 	static revokeObjectURL(url) {
-		const filePath = wx.getStorageSync(url);
+		/*const filePath = wx.getStorageSync(url);
 		const fs = wx.getFileSystemManager()
 		fs.removeSavedFile({
 			filePath
 		})
-		wx.removeStorageSync(url);
+		wx.removeStorageSync(url);*/
+		delete getApp().ObjectURL[url]
+
 	}
 }
