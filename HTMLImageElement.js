8bdc79c25c3d76e2469d68018c5ee4b1a25dd7f3
OneKit <onekit@onekit.cn>
2022-09-07 01:23:49 +0800

    .

--- HTMLImageElement.js
+++ HTMLImageElement.js
@@ -60,14 +60,18 @@ export default class HTMLImageElement extends EventTarget {
     this._src = src;
 
     if (src.startsWith("blob:")) {
-      const array = getApp().ObjectURL[src].array[0];
+      /*  const array = getApp().ObjectURL[src].array[0];
       this.wx_image.src =
-        "data:image/png;base64," + Base64.arrayBuffer2Base64(array);
-      /* const filePath = wx.getStorageSync(src);
-      const fs = wx.getFileSystemManager()
-      const base64 = fs.readFileSync(filePath, 'base64', 0)
-      console.error(filePath)
-      this.wx_image.src = 'data:image/png;base64,' +base64*/
+        "data:image/png;base64," + Base64.arrayBuffer2Base64(array);*/
+      const filePath = wx.getStorageSync(src);
+      const fs = wx.getFileSystemManager();
+      const base64 = fs.readFileSync(filePath, "base64", 0);
+      if (fs.accessSync(filePath)) {
+        fs.removeSavedFile({
+          filePath,
+        });
+      }
+      this.wx_image.src = "data:image/png;base64," + base64;
     } else {
       this.wx_image.src = src;
     }
