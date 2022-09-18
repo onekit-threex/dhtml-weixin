631030c2e95933ed7e6faf0df032409f1abc9d98
OneKit <onekit@onekit.cn>
2022-09-18 15:31:15 +0800

    .

--- Response.js
+++ Response.js
@@ -2,69 +2,84 @@
 /* eslint-disable no-console */
 import Base64 from './core/Base64'
 import Page from './core/Page'
+import Blob from "./Blob"
 export default class Response {
-  constructor(body, options = {}, request) {
-    if (!request) {
-      this.body = body
-      this.status = options.status
-      this.statusText = options.statusText
-      this.headers = options.headers
-    }
-    this.status = 200
-    this.request = request
-  }
+	constructor(body, options = {}, request) {
+		if (!request) {
+			this.body = body
+			this.status = options.status
+			this.statusText = options.statusText
+			this.headers = options.headers
+		}
+		if (typeof request == "string") {
+			this.request = {
+				url: request
+			}
+		} else {
+			this.request = request
+		}
+		this.status = 200
+	}
 
-  _run(responseType, dataType = 'text') {
+	_run(responseType, dataType = 'text') {
 		const onekit_debug = Page.getApp().onekit_debug
-    if(onekit_debug){
-      console[onekit_debug]('[fetch]', this.request.url, responseType, dataType)
-    }
-    if (this.request.url.endsWith('.js')) {
-      return new Promise((resolve) => {
-        resolve(this.request.url)
-      })
-    }
-    if (this.request.url.startsWith('data:')) {
-      return new Promise((resolve) => {
-        console.error("[fetch] base64 ??????????????????")
-        const BASE64 = 'base64,'
-        const url = this.request.url.substring(this.request.url.indexOf(BASE64) + BASE64.length)
-        resolve(Base64.base64ToArrayBuffer(url))
-      })
-    }
-    // /////////////////////////
-    return new Promise((resolve,reject) => {
-      Page.wx_request({
-        url: this.request.url,
-        headers: ((this.request.options || {}).headers || {}).data || {},
-        responseType,
-        dataType,
-        success(res) {
-          resolve(res.data)
-        },
-        fail: (e) => {
-          console.error('[fetch.fail]', this.request.url,e)
-          // eslint-disable-next-line prefer-promise-reject-errors
-           reject(null)
-        }
-      })
-    })
-  }
+		if (onekit_debug) {
+			console[onekit_debug]('[fetch]', this.request.url, responseType, dataType)
+		}
+		if (this.request.url.endsWith('.js')) {
+			return new Promise((resolve) => {
+				resolve(this.request.url)
+			})
+		}
+		if (this.request.url.startsWith('data:')) {
+			return new Promise((resolve) => {
+				console.error("[fetch] base64 ??????????????????")
+				const BASE64 = 'base64,'
+				const url = this.request.url.substring(this.request.url.indexOf(BASE64) + BASE64.length)
+				resolve(Base64.base64ToArrayBuffer(url))
+			})
+		}
+		// /////////////////////////
+		return new Promise((resolve, reject) => {
+			Page.wx_request({
+				url: this.request.url,
+				headers: ((this.request.options || {}).headers || {}).data || {},
+				responseType,
+				dataType,
+				success: (res) => {
+					resolve(res.data)
+				},
+				fail: (e) => {
+					console.error('[fetch.fail]', this.request.url, e)
+					// eslint-disable-next-line prefer-promise-reject-errors
+					reject(null)
+				}
+			})
+		})
+	}
 
-  arrayBuffer() {
-    return this._run('arraybuffer')
-  }
+	arrayBuffer() {
+		return this._run('arraybuffer', 'arraybuffer')
+	}
 
-  // eslint-disable-next-line class-methods-use-this
-  blob() {
-    throw new Error('[fetch]blob')
-  }
+	blob() {
+		return new Promise((callback, reject) => {
 
-  text() {
-    return this._run('text')
-  }
+			this._run('arraybuffer', 'blob').then(data => {
+				callback(new Blob([data]));
+			}).catch((e) => {
+				console.error(e)
+				reject(e.message)
+			})
 
-  json() {
-    return this._run('text', 'json')
-  }
+		})
+	}
+
+	text() {
+		return this._run('text')
+	}
+
+	json() {
+		return this._run('text', 'json')
+	}
 }
--- createImageBitmap.js
+++ createImageBitmap.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import HTMLImageElement from "./HTMLImageElement"
 import Page from "./core/Page"
+import Base64 from "./core/Base64"
+import Blob from "./Blob"
 
 export default function createImageBitmap(src, options) {
   return new Promise((resolve) => {
@@ -8,6 +10,9 @@ export default function createImageBitmap(src, options) {
     const img = new HTMLImageElement(canvas)
     img.onload = function() {
       resolve(img)
+    }
+    if(src instanceof Blob){
+        src = "data:image/png;base64,"+Base64.arrayBufferToBase64(src.array[0])
     }
     img.src = src
   })
