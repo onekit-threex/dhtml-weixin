7060f967c8220cbe50a07343b0f2be8453f53000
OneKit <onekit@onekit.cn>
2022-12-08 14:06:07 +0800

    Update Event0.js

--- Event0.js
+++ Event0.js
@@ -1,6 +1,3 @@
-var systemInfo = wx.getSystemInfoSync()
-var platform = systemInfo.platform
-var pixelRatio = systemInfo.pixelRatio
 export default class Event {
 	static fix(mini_e) {
 		mini_e.preventDefault = () => {}
