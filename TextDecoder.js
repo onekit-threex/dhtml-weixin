f092c2766fe9484df14099f73946637f895ff473
OneKit <onekit@onekit.cn>
2023-03-23 20:05:53 +0800

    .

--- TextDecoder.js
+++ TextDecoder.js
@@ -1,3 +1,28 @@
-import encoding from './core/encoding/encoding.js'
-module.exports = encoding.TextDecoder
+//import encoding from './core/encoding/encoding.js'
+//module.exports = encoding.TextDecoder
+class TextDecoder{
+    decode(array){
+  
+		let s = '';
+
+		for ( let i = 0, il = array.length; i < il; i ++ ) {
+
+			// Implicitly assumes little-endian.
+			s += String.fromCharCode( array[ i ] );
+
+		}
+
+		try {
+
+			// merges multi-byte utf-8 characters.
+
+			return decodeURIComponent( escape( s ) );
+
+		} catch ( e ) { // see #16358
+
+			return s;
+
+		}
+    }
+}
 module.exports = TextDecoder
